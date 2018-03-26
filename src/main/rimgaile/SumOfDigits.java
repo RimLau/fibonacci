d259f9f49b883acfccbe267f305a158b6dc61899
AzureAD\RimgaileLauzadyte <rimgaile.lauzadyte@devbridge.com>
2018-03-26 10:45:52 +0300

    Write into console method was created

--- src/main/rimgaile/SumOfDigits.java
+++ src/main/rimgaile/SumOfDigits.java
@@ -1,22 +1,15 @@
 package rimgaile;
 
 import java.io.*;
-import java.util.Scanner;
 
 public class SumOfDigits {
     public static void main(String[] args) {
-        //Scanner input = new Scanner(System.in);
-        //System.out.println("Enter integer number:");
-        //int number = input.nextInt();
-        //System.out.println(sumOfNumber(number));
-
-        int value = file();
-        System.out.println("lala: " + value);
-        System.out.println(sumOfNumber(value));
+        int value = readFromFile();
+        writeIntoConsole(value);
 
     }
 
-    public static int file() {
+    public static int readFromFile() {
         BufferedReader br = null;
         int value = 0;
         String line;
@@ -40,6 +33,10 @@ public class SumOfDigits {
         return value;
     }
 
+    public static void writeIntoConsole (int value) {
+        System.out.println("lala: " + value);
+        System.out.println(sumOfNumber(value));
+    }
     public static int sumOfNumber(int value) {
         int sum = 0;
         while (value != 0) {
