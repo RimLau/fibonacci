53db7b16461d1c11f05a9122a025f597aa21e8b4
AzureAD\RimgaileLauzadyte <rimgaile.lauzadyte@devbridge.com>
2018-03-26 17:04:16 +0300

    Code was rewritten

--- src/main/rimgaile/Calculator.java
+++ src/main/rimgaile/Calculator.java
@@ -6,45 +6,32 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int num1 = readFirstNum(input);
-        int num2 = readSecondNum(input);
-        String operator = readOperator(input);
-
-        writeResultIntoConsole(num1, num2, operator);
-
+        int num1 = readNumber(input, "Enter Your first number");
+        int num2 = readNumber(input, "Enter your second number");
+        String operator = readString(input, "Enter operator:");
+        System.out.println("Result: " + getResult(num1, num2, operator));
     }
 
-    public static void writeResultIntoConsole(int num1, int num2, String operator) {
-        int results = 0;
+    public static int getResult(int num1, int num2, String operator) {
+        int result = 0;
         try {
-            results = calculate(num1, num2, operator);
+            result = calculate(num1, num2, operator);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-
-        System.out.println(results);
+        return result;
     }
 
-    public static int readFirstNum(Scanner input) {
-        System.out.println("Enter your first number:");
-        int num1 = input.nextInt();
-
-        return num1;
+    public static int readNumber(Scanner input, String text) {
+        System.out.println(text);
+        return input.nextInt();
     }
 
-    public static int readSecondNum(Scanner input) {
-        System.out.println("Enter your second number:");
-        int num2 = input.nextInt();
-
-        return num2;
-    }
-
-    public static String readOperator(Scanner input) {
-        System.out.println("Enter operator:");
-        String operator = input.next();
-        return operator;
+    public static String readString(Scanner input, String text) {
+        System.out.println(text);
+        return input.next();
     }
 
     public static int calculate(int num1, int num2, String operator) throws Exception {
